120f493985c0d17fef15afa6da7b00d79079da4c
Ruhanjali <anjalitomarbk4@gmail.com>
2022-02-25 19:06:23 +0530

    tanuuser

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,11 +5,23 @@ const control = require('../controller/user.ctrl')
 
 router.get('/',control.userHomePage);
 router.get('/userLogin',control.userLoginPage);
+router.post('/userLogin',control.userLoginPost);
+
 router.get('/user-register',control.userRegisterPage);
+router.post('/user-register',control.userRegisterPost);
+router.get("/signout",control.signout);
 
 router.get('/MensSection',control.userProductsPage);
 router.get('/WomenSection',control.userProductsPage);
 router.get('/KidsSection',control.userProductsPage);
 router.get('/NewArrival',control.newArrivalPage);
+
+
+module.exports = router;
+
+
+
+
+
 
 module.exports = router;
