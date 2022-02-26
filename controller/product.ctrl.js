2f259ae619cf0ae8b767a0a83ebda04c81524717
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 <saipriyanshu786@gmail.com>
2022-02-26 15:44:48 +0530

    product list by category ID

--- controller/product.ctrl.js
+++ controller/product.ctrl.js
@@ -3,66 +3,66 @@ const path = require("path");
 const { render } = require('express/lib/response');
 const { request } = require('http');
 const Product = require('../model/product')
-exports.saveProduct  = (request,response,next)=>{
+exports.saveProduct = (request, response, next) => {
     let product = new Product();
     product.frontViewImage = "";
     product.backViewImage = "";
     product.leftViewImage = "";
     product.rightViewImage = "";
-    if(request.files.length>0){
-       product.frontViewImage = request.files[0].filename;
-       if(request.files.length>1){
-           product.backViewImage = request.files[1].filename;
-           if(request.files.length>2){
-               product.leftViewImage = request.files[2].filename;
-               if(request.files.length>3){
-                   product.rightViewImage = request.files[3].filename;
-               }
-           }
-       }
+    if (request.files.length > 0) {
+        product.frontViewImage = request.files[0].filename;
+        if (request.files.length > 1) {
+            product.backViewImage = request.files[1].filename;
+            if (request.files.length > 2) {
+                product.leftViewImage = request.files[2].filename;
+                if (request.files.length > 3) {
+                    product.rightViewImage = request.files[3].filename;
+                }
+            }
+        }
     }
-    product.category_id  = request.body.category_id;
+    product.category_id = request.body.category_id;
     product.name = request.body.name;
     product.quantity = request.body.quantity;
     product.description = request.body.description;
     product.price = request.body.price;
-  
+
     product.save()
-    .then(()=>{
-        return response.render("../Views/Admin-Pages/admin-home.ejs");
-    })
-    .catch(err=>{
-        console.log(err);
-        return response.send("Error....");
-    });
+        .then(() => {
+            return response.render("../Views/Admin-Pages/admin-home.ejs");
+        })
+        .catch(err => {
+            console.log(err);
+            return response.send("Error....");
+        });
 }
-exports.editProduct = (request,response)=>{
-    let product= new Product();
+exports.editProduct = (request, response) => {
+    let product = new Product();
     const id = request.params.id;
     product.getproductById(id).then(
         (result) => {
-   console.log(result);
-          console.log(result);
-           return response.render("../Views/Admin-Pages/edit-product.ejs",{
-                ProductId:result
-            });    
+            console.log(result);
+            console.log(result);
+            return response.render("../Views/Admin-Pages/edit-product.ejs", {
+                ProductId: result
+            });
 
         }
     ).catch();
 
 }
 
-exports.editProductPost=(request,response)=>{
+exports.editProductPost = (request, response) => {
     let product = new Product();
     const id = request.params.id;
     console.log(id);
-    console.log( "req body "+ request.body);
+    console.log("req body " + request.body);
     product.name = request.body.name;
     product.quantity = request.body.quantity;
     product.price = request.body.price;
-    product.description=request.body.description;
-    product.update(id).then(result=>{
-     response.redirect("/product/product-list");
+    product.description = request.body.description;
+    product.update(id).then(result => {
+        response.redirect("/product/product-list");
     }).catch();
 }
 
@@ -81,22 +81,22 @@ exports.addProductPage = (request, response, next) => {
             return response.send("Erro.....");
         });
 };
-exports.productListPage=(request,response,next)=>{
-    let product=new Product();
+exports.productListPage = (request, response, next) => {
+    let product = new Product();
     Product.fatchAll()
-    .then(results => {
-       
-      response.render("../Views/Admin-Pages/product-list.ejs", {
-             productList: results
-         });
-    })
-    .catch(err => {
-        console.log(err);
-    });
+        .then(results => {
+
+            response.render("../Views/Admin-Pages/product-list.ejs", {
+                productList: results
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
 
 }
-exports.deleteProduct=(request,response,next)=>{
-  let product = new Product();
+exports.deleteProduct = (request, response, next) => {
+    let product = new Product();
     const id = request.params.id;
     Product.delete(id).then(
         () => {
@@ -105,4 +105,38 @@ exports.deleteProduct=(request,response,next)=>{
     ).catch();
 
 
+}
+
+exports.selectCategory =(request,response,next)=>{
+    Promise.all([Category.fetchAll(), Product.fatchAll()])
+    .then(results=>{
+        return response.render("../Views/Admin-Pages/product-list.ejs",{
+            categoryList: results[0],
+            productList: results[1]
+        });
+    })
+    .catch(err=>{
+        console.log(err);
+        return response.send("Error.....");
+    });
+    
+
+}
+
+
+
+exports.productByCategoryId =(request,response,next)=>{
+    let category_id=request.params.id;
+    console.log(request.body);
+    Promise.all([Category.fetchAll(), Product.fatchAllById(category_id)])
+    .then(results=>{
+        return response.render("../Views/Admin-Pages/product-list.ejs",{
+            categoryList: results[0],
+            productList: results[1]
+        });
+    })
+    .catch(err=>{
+        console.log(err);
+        return response.send("Error.....");
+    });
 }
